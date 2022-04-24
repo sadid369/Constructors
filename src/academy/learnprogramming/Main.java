1c8ab92c79f9ae5299ff68231ecac008e6381d05
Sadid369 <sadid.jones@gmail.com>
2022-04-24 23:43:58 +0600

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Account bobsAccount = new Account();
+
+    bobsAccount.withdrawal(100);
+
+bobsAccount.deposit(50);
+    bobsAccount.withdrawal(100);
+bobsAccount.deposit(51);
+    bobsAccount.withdrawal(100);
+    }
+}
